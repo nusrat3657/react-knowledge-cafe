a3653aab8518210f0b679edb8155d0492a7c94c7
nusrat3657 <nusratjahan3657r@gmail.com>
2024-03-13 03:25:37 +0600

    added author

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,10 @@
+import profile from '../../assets/img/profile.png'
 const Header = () => {
     return (
-        <div>
+        <header className='flex justify-between p-4 mx-10 border-b-2'>
             <h1 className='text-4xl font-bold exo-2'>Knowledge Cafe</h1>
-        </div>
+            <img className='w-[60px] h-[50px]' src={profile} alt="" />
+        </header>
     );
 };
 
