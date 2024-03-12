20073f2a1284e523626b8810412fe49024ff945d
nusrat3657 <nusratjahan3657r@gmail.com>
2024-03-13 03:50:04 +0600

    added name and others

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,9 +1,28 @@
 import PropTypes from 'prop-types';
 const Blog = ({ blog }) => {
-    console.log(blog);
+    const {title, cover, author, author_img, reading_time,posted_date, hashtags} = blog;
     return (
         <div>
-
+            <img src={cover} alt={`Cover picture of the title ${title}`} />
+            <div className='flex justify-between'>
+                <div className='flex'>
+                    <img className='w-[60px] h-[50px]' src={author_img} alt="" />
+                    <div className='ml-5'>
+                        <h5 className='text-2xl font-bold'>{author}</h5>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+                    
+                </div>
+            </div>
+            <h2 className="text-4xl">{title}</h2>
+            <p>
+                {
+                    hashtags.map((hash, idx) => <span key={idx}><a href=''>#{hash}</a></span>)
+                }
+            </p>
         </div>
     );
 };
